153d086f52eb7c0b1b06d1131a2640f47aecda79
Artli22 <ludinlima2514@gmail.com>
2026-04-20 16:41:57 -0600

    Agregar y configurar db

--- db.go
+++ db.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "modernc.org/sqlite"
+)
+
+var db *sql.DB
+
+func initDB() {
+	var err error
+	db, err = sql.Open("sqlite", "series2.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+}
--- models.go
+++ models.go
@@ -1,1 +1,10 @@
 package main
+
+type Series struct {
+	ID             int    `json:"id"`
+	Name           string `json:"name"`
+	Description    string `json:"description"`
+	ImageURL       string `json:"image_url"`
+	CurrentEpisode int    `json:"current_episode"`
+	TotalEpisodes  int    `json:"total_episodes"`
+}
